2f67767cce6b0d1eb5ac96caa7f6c7d41fb4d747
TaniaLebed <lebedtania666@gmail.com>
2020-06-21 20:28:17 +0300

    Added routing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,11 +4,14 @@ import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ErrorBoundry from '../error-boundry';
 import {PeoplePage, PlanetsPage, StarshipsPage} from "../pages";
+import StarshipDetails from "../sw-components/starship-details";
 
 import {SwapiServiceProvider} from "../swapi-service-context/swapi-service-context";
 
 import './app.css';
 import SwapiService from "../../services/swapi-service";
+
+import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
 
 export default class App extends Component {
 
@@ -18,14 +21,28 @@ export default class App extends Component {
         return (
             <ErrorBoundry>
                 <SwapiServiceProvider value={this.swapiService}>
-                    <div className="stardb-app">
-                        <Header />
-                        <RandomPlanet />
+                    <Router>
+                        <div className="stardb-app">
+                            <Header />
+                            <RandomPlanet />
 
-                        <PeoplePage/>
-                        <PlanetsPage/>
-                        <StarshipsPage/>
-                    </div>
+                            <Switch>
+                                <Route path="/"
+                                       render={() => <h2>Welcome to StarDB</h2>}
+                                       exact />
+                                <Route path="/people/:id?" component={PeoplePage}/>
+                                <Route path="/planets/" component={PlanetsPage}/>
+                                <Route path="/starships/"
+                                       component={StarshipsPage}
+                                       exact />
+                                <Route path="/starships/:id" render={({match}) => {
+                                    const {id} = match.params;
+                                    return <StarshipDetails itemId={id}/>
+                                }}/>
+                                <Route render={() => <h2>Page not found</h2>}/>
+                            </Switch>
+                        </div>
+                    </Router>
                 </SwapiServiceProvider>
             </ErrorBoundry>
         );
