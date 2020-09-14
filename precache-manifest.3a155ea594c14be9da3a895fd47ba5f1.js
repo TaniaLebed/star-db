bdcfe5a051ee8d15a53f5e50eea10497a2a10503
TaniaLebed <lebedtania666@gmail.com>
2020-09-14 10:48:39 +0300

    Updates

---
+++ precache-manifest.3a155ea594c14be9da3a895fd47ba5f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f72a9c94bc030942b8228653272330e",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "e879408843b56da5608c",
+    "url": "/star-db/static/css/main.0e2f49bc.chunk.css"
+  },
+  {
+    "revision": "7f5827de60b7e572240b",
+    "url": "/star-db/static/js/2.c7c1d051.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/star-db/static/js/2.c7c1d051.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e879408843b56da5608c",
+    "url": "/star-db/static/js/main.65918f19.chunk.js"
+  },
+  {
+    "revision": "5e9fe22e0f74b24b7463",
+    "url": "/star-db/static/js/runtime-main.d9f21290.js"
+  }
+]);
